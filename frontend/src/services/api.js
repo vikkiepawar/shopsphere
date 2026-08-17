b83a904c3207f21657e48f30d7dbb1cbb8f1d07f
Vikram Pawar <pawarvikram2004@gmail.com>
2026-08-17 12:41:40 +0530

    Connect frontend to live backend API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://shopsphere-api-swec.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
